4a3e5f868893b7c9a172cb8fd65f1c3523ef640a
taiki72 <tkmy_max@hotmail.com>
2019-04-18 02:29:09 +0900

    test

---
+++ WebApplication2/Controllers/TestPageController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestPage.Controllers
+{
+    public class TestPageController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            string message = "Hello " + name + ", NumTimes is: " + numTimes;
+            return "" + Server.HtmlEncode(message) + "";
+        }
+    }
+}
